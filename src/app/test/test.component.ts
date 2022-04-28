94a3bd62ad17a1f98e688e09bfcfe7785b802596
Milon Roy <mnroy293@gmail.com>
2022-04-29 02:14:56 +0600

    style binding done

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   public user = "milon";
   public siteUrl = window.location.href;
+  public myId = "test id";
+
+  // class binding
+  public PinkColor = "pink-color";
+  public orangeColor = "orange-color";
+  public special = "text-Special";
+  public hasEror = false;
+  // multiClass
+  public messageClass = {
+    "pink-color": !this.hasEror,
+    "orange-color": this.hasEror,
+    "text-Special": this.special,
+  }
+  // style bindding
+  public highLightColor = "hotPink";
+  public multiStyle = {
+    fontStyle: "italic",
+    fontWeith: "bold",
+    backgroundColor: "orange",
+  }
+
 
   constructor() { }
 
